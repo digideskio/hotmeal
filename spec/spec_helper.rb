c63468577b60441ecc6d6e2eb3eae69390f37907
Alex Semyonov <al@semyonov.us>
2016-01-04 00:11:21 +0300

    Fix RSpec Suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,8 @@ require 'hotmeal'
 Dir[File.join(SPEC_ROOT, 'spec/support/**/*.rb')].each {|f| require f}
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.mock_with :rspec
+  config.expect_with(:rspec) { |c| c.syntax = [:should, :expect] }
 end
