9f5d41027c588db82e1a53f203955f65845510f8
Alex Semyonov <al@semyonov.us>
2016-01-18 20:55:07 +0300

    Inspect attributes, collections, html nodes

--- lib/hotmeal/mapper/inspectable.rb
+++ lib/hotmeal/mapper/inspectable.rb
@@ -4,13 +4,23 @@ module Hotmeal
   module Mapper
     module Inspectable
       def inspect
-        '#<%s:0x%x %s>' % [self.class, object_id, inspect_attributes]
+        if has_attributes?
+          contents = inspect_attributes
+          '#<%s:0x%x %s>' % [self.class, object_id, contents]
+        elsif respond_to?(:collection)
+          collection.inspect
+        elsif respond_to?(:html_content)
+          html_content.inspect
+        else
+          super
+        end
       end
 
       private
 
       def inspect_attributes
         attributes.map do |(name, value)|
+          value = value.__getobj__ while value.respond_to?(:__getobj__)
           "@#{name}=#{value.inspect}"
         end.join(' ')
       end
