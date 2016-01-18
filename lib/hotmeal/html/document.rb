db9aeda7495acca1e23ef24ae16bd3d696d758c5
Alex Semyonov <al@semyonov.us>
2016-01-18 23:11:46 +0300

    Delegate attributes (with setter and checker)

--- lib/hotmeal/html/document.rb
+++ lib/hotmeal/html/document.rb
@@ -4,14 +4,8 @@ require 'hotmeal/mapper/decorator'
 module Hotmeal
   module Html
     class Document < Hotmeal::Html::Html
-      # @return [String]
-      delegate :title, to: :head
-      # @return [Hotmeal::Html::MetaData]
-      delegate :meta, to: :head
-      # @return [Array<String>] array of keywords
-      delegate :keywords, to: :meta
-      # @return [String] description
-      delegate :description, to: :meta
+      delegate_attribute :title, :meta, to: :head
+      delegate_attribute :keywords, :description, to: :meta
 
       def process
         super
--- lib/hotmeal/mapper/abstract_decorator.rb
+++ lib/hotmeal/mapper/abstract_decorator.rb
@@ -9,6 +9,15 @@ module Hotmeal
       include Hotmeal::Mapper::Inspectable
 
       alias_method :decorated, :__getobj__
+
+      def self.delegate_attribute(*attributes)
+        options = attributes.extract_options!
+        attributes = attributes.inject([]) do |result, attribute|
+          result + %I(#{attribute} #{attribute}= #{attribute}?)
+        end
+        attributes << options
+        delegate(*attributes)
+      end
     end
   end
 end
