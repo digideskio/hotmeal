69e03f4bfe33ee10f3f684c2713f67dbfdc45dea
Alex Semyonov <al@semyonov.us>
2016-01-19 01:51:00 +0300

    Rename OpenGraph structures classes

--- lib/hotmeal/open_graph/video.rb
+++ lib/hotmeal/open_graph/video.rb
@@ -2,10 +2,12 @@ require 'hotmeal/open_graph/structured_property'
 
 module Hotmeal
   class OpenGraph
-    class AudioProperty < StructuredProperty
+    class Video < StructuredProperty
       property :url
       property :secure_url
       property :type
+      property :width, type: Integer
+      property :height, type: Integer
     end
   end
 end
