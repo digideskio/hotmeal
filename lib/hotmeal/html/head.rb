55849ac4ebcec9e3f1365a4897bcbb2d84c419e3
Alex Semyonov <al@semyonov.us>
2016-01-18 18:24:03 +0300

    Decorate Head#base_uri as URI

--- lib/hotmeal/html/head.rb
+++ lib/hotmeal/html/head.rb
@@ -4,9 +4,18 @@ module Hotmeal
   module Html
     class Head < Mapper::Decorator
       element :title
-      attribute '/base/@href', as: :base_uri
+      attribute '/base[@href]/@href', as: :base_uri
       elements :meta, class: MetaData
       elements :link, class: Links, as: :links
+
+      def base_uri
+        @base_uri ||= (uri = super) ? URI(uri.to_s) : nil
+      end
+
+      def base_uri=(uri)
+        remove_instance_variable(:@base_uri)
+        super(uri)
+      end
     end
   end
 end
