e08c02075db5ff61baf03a42d9a03203399b4e57
Alex Semyonov <al@semyonov.us>
2016-01-20 23:46:01 +0300

    Reimplement Slim representation based on delegator

--- lib/hotmeal/html/slim_representation.rb
+++ lib/hotmeal/html/slim_representation.rb
@@ -4,84 +4,110 @@ require 'nokogiri'
 module Hotmeal
   module Html
     module SlimRepresentation
-      def to_s
-        case self
-        when Nokogiri::XML::Element
-          string = "#{[element, id, classes, role].compact.join}#{attributes}"
-          if children.size > 1
-            "#{string}\n#{indent(inspect_children)}"
-          elsif children.size == 1
-            if children.first.is_a?(Nokogiri::XML::Element)
-              "#{string}: #{inspect_children}"
+      class Slim < SimpleDelegator
+        HIDDEN_ATTRIBUTES = %i(align bgcolor cellpadding cellspasing clear width)
+
+        @@mappings = {
+          id: ->(element) { "##{element[:id]}" },
+          class: ->(element) { '.' + element.classes.join('.') },
+          role: ->(element) { "@#{element[:role]}" + element.classes.join('.') }
+        }
+
+        def self.decorate(node)
+          new(node)
+        end
+
+        def classes
+          @classes ||= self[:class].to_s.split(/\s+/)
+        end
+
+        def to_slim(options = {})
+          case __getobj__
+          when Nokogiri::XML::Text
+            return unless text_content.present?
+            if self.next || self.previous
+              "| #{text_content}" if text_content.present?
             else
-              "#{string} #{content}"
+              text_content
             end
           else
-            string
+            [options[:prefix], definition, attributes, contents].compact.join(' ')
           end
-        when Nokogiri::XML::Text
-          "| #{content}" if content.present?
-        else
-          nil
         end
-      end
 
-      def element
-        if name == 'div' then
-          if id? || html_classes.any? || role?
+        private
+
+        def text_content
+          self.inner_text if self.inner_text.present?
+        end
+
+        def definition
+          [element_name, aliased_attributes].compact.join
+        end
+
+        def element_name
+          name if element_name_visible?
+        end
+
+        def element_name_visible?
+          name != 'div' || aliased_attributes_present?
+        end
+
+        def aliased_attributes
+          @aliased_attributes ||= @@mappings.map do |(attribute, mapping)|
+            mapping.call(self) if self[attribute]
+          end.compact.join
+        end
+
+        def aliased_attributes_present?
+          aliased_attributes.present?
+        end
+
+        def visible_attributes
+          @visible_attributes ||= keys - @@mappings.keys.map(&:to_s) - HIDDEN_ATTRIBUTES
+        end
+
+        def attributes
+          attributes = visible_attributes.map do |name|
+            "#{name}=#{self[name].inspect}"
+          end.join(' ')
+          attributes
+        end
+
+        def hidden_attribute?(name)
+          name = name.to_sym
+          @@mappings.keys.include?(name) || HIDDEN_ATTRIBUTES.include?(name)
+        end
+
+        def contents
+          children = decorated_children
+          if children.size == 1
+            children.first.to_slim(prefix: ':')
+          elsif children.size == 0
             nil
           else
-            'div'
+            indent(children.map(&:to_slim).compact.join("\n"))
           end
-        else
-          name
+        end
+
+        def indent(text, indentation = ' ')
+          text.to_s.split("\n").map do |line|
+            "\n#{indentation}#{line}"
+          end.join
+        end
+
+        def decorated_children
+          children.map do |child|
+            Slim.decorate(child)
+          end
         end
       end
 
-      def id
-        id? ? "##{self[:id]}" : nil
+      def to_slim
+        @slim ||= Slim.decorate(self).to_slim
       end
 
-      def id?
-        self[:id].present?
-      end
-
-      def classes
-        classes = html_classes.join('.')
-        ".#{classes}" if classes.present?
-        nil
-      end
-
-      def html_classes
-        self[:class].to_s.split(/\s+/)
-      end
-
-      def role
-        role? ? "@#{self[:role]}" : nil
-      end
-
-      def role?
-        self[:role].present?
-      end
-
-      def attributes
-        attributes = keys.map do |attribute|
-          "#{attribute}=#{self[attribute].inspect}" unless %w(id class role).include?(attribute)
-        end.compact.join(' ')
-        " #{attributes}" if attributes.present?
-      end
-
-      def inspect_children
-        children.map do |child|
-          child.extend(SlimRepresentation)
-          child.to_s
-        end.compact.join("\n")
-      end
-
-      def indent(text, blank = 2)
-        indentation = ' ' * blank
-        indentation << text.to_s.split(/\n/).join("\n" << indentation)
-      end
+      alias_method :to_s, :to_slim
     end
   end
 end
